20f7e10a9e4d0be33de0f8a86f71682802717832
bliss <yuanshouna@126.com>
2015-07-02 14:37:06 +0800

    create module construct

---
+++ core-service/src/main/java/me/bliss/kafka/core/service/ZookeeperServcie.java
@@ -0,0 +1,12 @@
+package me.bliss.kafka.core.service;
+
+/**
+ *
+ *
+ * @author lanjue
+ * @version $Id: me.bliss.kafka.core.service, v 0.1 7/1/15
+ *          Exp $
+ */
+public interface ZookeeperServcie {
+
+}
---
+++ core-service/src/main/java/me/bliss/kafka/core/service/impl/ZookeeperServiceImpl.java
@@ -0,0 +1,13 @@
+package me.bliss.kafka.core.service.impl;
+
+import me.bliss.kafka.core.service.ZookeeperServcie;
+
+/**
+ *
+ *
+ * @author lanjue
+ * @version $Id: me.bliss.kafka.core.service.impl, v 0.1 7/1/15
+ *          Exp $
+ */
+public class ZookeeperServiceImpl implements ZookeeperServcie{
+}
